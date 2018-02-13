007274100ea58e6fdbf168ec5e0a4f4225460bd4
apd911 <apd291@gmail.com>
2018-02-13 17:20:38 +0100

    Add files via upload

---
+++ LEDs.ino
@@ -0,0 +1,257 @@
+#include <FastLED.h>
+#include <EEPROM.h>
+ 
+#define LED_PIN 5
+#define NUM_LEDS 95
+#define LED_TYPE    WS2812B
+#define COLOR_ORDER GRB
+#define SPARKING 120
+#define COOLING  55
+
+bool gReverseDirection = false;
+
+int redc = EEPROM.read(11);
+int greenc = EEPROM.read(13);
+int bluec = EEPROM.read(15);
+
+long c[4];
+ 
+struct CRGB leds[NUM_LEDS];
+
+int UPDATES_PER_SECOND = EEPROM.read(8);
+
+CRGBPalette32 currentPalette;
+TBlendType    currentBlending;
+
+int paletteCounter = EEPROM.read(5);
+int brightnessCounter = EEPROM.read(6);
+int dir = EEPROM.read(10);
+ 
+void setup() {
+
+  Serial.begin(9600);
+ 
+  delay( 1000 );
+ 
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(255);
+  FastLED.clear();  
+}
+ 
+void loop() {
+
+  if (Serial.available() >= 4) {
+    for (int i = 0; i < 4; i++) {
+      c[i] = Serial.read();
+    }
+    Serial.flush();
+    if (c[0] == 9 && c[1] == 1) {
+      Serial.println(String(paletteCounter) + ' ' + String(brightnessCounter) + ' ' + String(UPDATES_PER_SECOND) + ' ' + String(dir));
+    }
+    else if (c[0] == 1 && c[1] == 2) {
+      brightnessCounter=c[2];
+    }
+    else if (c[0] == 1 && c[1] == 3) {
+      if (dir == 0) {
+        dir = 1;
+      }
+      else {
+        dir = 0; //invert direction
+      }
+    }
+    else if (c[0] == 1 && c[1] == 4) {
+      UPDATES_PER_SECOND=c[2];
+    }
+    
+    else if (c[0] == 2 && c[1] == 1) {
+      paletteCounter = 1; //rainbow
+    }
+    else if (c[0] == 2 && c[1] == 2) {
+      paletteCounter = 2; //blackwhite
+    }
+    else if (c[0] == 2 && c[1] == 3) {
+      paletteCounter = 3; //fire
+    }
+    else if (c[0] == 2 && c[1] == 4) {
+      paletteCounter = 4; //ocean
+    }
+    else if (c[0] == 2 && c[1] == 5) {
+      paletteCounter = 5; //lava
+    }
+    else if (c[0] == 2 && c[1] == 6) {
+      paletteCounter = 6; //forest
+    }
+    else if (c[0] == 2 && c[1] == 7) {
+      paletteCounter = 7; //greenpurple
+    }
+    else if (c[0] == 2 && c[1] == 8) {
+      paletteCounter = 8; //police
+    }
+    else if (c[0] == 2 && c[1] == 9) {
+      paletteCounter = 9; //party
+    }
+    else if (c[0] == 2 && c[1] == 0) {
+      paletteCounter = 10; //blackout
+    }
+    else if (c[0] == 3 && c[1] == 0) {
+      EEPROM.write(5, paletteCounter);
+      EEPROM.write(6, brightnessCounter);
+      EEPROM.write(8, UPDATES_PER_SECOND);
+      EEPROM.write(10, dir);
+      EEPROM.write(11, redc);
+      EEPROM.write(13, greenc);
+      EEPROM.write(15, bluec); //save settings
+    }
+    else if (c[0] == 4){
+      paletteCounter = 11; //custom color
+      redc = c[1];
+      greenc = c[2];
+      bluec = c[3];
+    }
+    
+  }
+  ChangePalette();
+  static uint8_t startIndex = 0;
+
+  startIndex = startIndex + 1;
+   
+  FillLEDsFromPaletteColors(startIndex);
+  FastLED.show();
+  FastLED.delay(800 / UPDATES_PER_SECOND);  
+}
+
+void fillSolid(uint32_t colour) {      
+  fill_solid(leds, NUM_LEDS, colour);
+}
+ 
+void FillLEDsFromPaletteColors( uint8_t colorIndex)
+{
+  
+  for( int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = ColorFromPalette( currentPalette, colorIndex, brightnessCounter, currentBlending);
+    if (dir == 0) {
+      colorIndex += 3;
+    }
+    else {
+      colorIndex -= 3;
+    }
+  }
+}
+ 
+void ChangePalette()
+{   
+    if( paletteCounter == 1)  { currentPalette = RainbowColors_p;         currentBlending = LINEARBLEND;}
+    if( paletteCounter == 2)  { SetupBlackAndWhiteStripedPalette();       currentBlending = LINEARBLEND;}
+    if( paletteCounter == 3)  { Fire2012();                               currentBlending = LINEARBLEND;}
+    if( paletteCounter == 4)  { currentPalette = OceanColors_p;           currentBlending = LINEARBLEND;}
+    if( paletteCounter == 5)  { currentPalette = LavaColors_p;            currentBlending = LINEARBLEND;}
+    if( paletteCounter == 6)  { currentPalette = ForestColors_p;          currentBlending = LINEARBLEND;}
+    if( paletteCounter == 7)  { SetupPurpleAndGreenPalette();             currentBlending = LINEARBLEND;}
+    if( paletteCounter == 8)  { SetupPolicePalette();                     currentBlending = LINEARBLEND;}
+    if( paletteCounter == 9)  { currentPalette = PartyColors_p;           currentBlending = LINEARBLEND;}
+    if( paletteCounter == 10)  { Black();                                 currentBlending = LINEARBLEND;}
+    if( paletteCounter == 11)  { fill_solid( currentPalette, 32, CRGB(redc, greenc, bluec)); currentBlending = LINEARBLEND;}
+}
+
+void SetupBlackAndWhiteStripedPalette()
+{
+  fill_solid( currentPalette, 16, CRGB::White);
+  currentPalette[7] = CRGB::Black;
+  currentPalette[8] = CRGB::Black;
+  currentPalette[9] = CRGB::Black;
+  currentPalette[22] = CRGB::White;
+  currentPalette[23] = CRGB::White;
+  currentPalette[24] = CRGB::White;
+}
+
+void SetupPolicePalette()
+{
+  fill_solid( currentPalette, 32, CRGB::Black);
+  currentPalette[0] = CRGB::Red;
+  currentPalette[1] = CRGB::Red;
+  currentPalette[2] = CRGB::Black;
+  currentPalette[3] = CRGB::Blue;
+  currentPalette[4] = CRGB::Blue;
+  currentPalette[5] = CRGB::Black;
+  currentPalette[6] = CRGB::Red;
+  currentPalette[7] = CRGB::Red;
+  currentPalette[8] = CRGB::Black;
+  currentPalette[9] = CRGB::Blue;
+  currentPalette[10] = CRGB::Blue;
+  currentPalette[11] = CRGB::Black;
+  currentPalette[12] = CRGB::Red;
+  currentPalette[13] = CRGB::Red;
+  currentPalette[14] = CRGB::Red;
+  currentPalette[15] = CRGB::Red;
+  currentPalette[16] = CRGB::Black;
+  currentPalette[17] = CRGB::Blue;
+  currentPalette[18] = CRGB::Blue;
+  currentPalette[19] = CRGB::Blue;
+  currentPalette[20] = CRGB::Blue;
+  currentPalette[21] = CRGB::Black;
+  currentPalette[22] = CRGB::Red;
+  currentPalette[23] = CRGB::Red;
+  currentPalette[24] = CRGB::Red;
+  currentPalette[25] = CRGB::Red;
+  currentPalette[26] = CRGB::Black;
+  currentPalette[27] = CRGB::Blue;
+  currentPalette[28] = CRGB::Blue;
+  currentPalette[29] = CRGB::Blue;
+  currentPalette[30] = CRGB::Blue;
+  currentPalette[31] = CRGB::Black;
+}
+
+void Fire2012()
+{
+  static byte heat[NUM_LEDS];
+
+    for( int i = 0; i < NUM_LEDS; i++) {
+      heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
+    }
+  
+    for( int k= NUM_LEDS - 1; k >= 2; k--) {
+      heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
+    }
+    
+    if( random8() < SPARKING ) {
+      int y = random8(7);
+      heat[y] = qadd8( heat[y], random8(160,255) );
+    }
+
+    for( int j = 0; j < NUM_LEDS; j++) {
+      CRGB color = HeatColor( heat[j]);
+      int pixelnumber;
+      if( gReverseDirection ) {
+        pixelnumber = (NUM_LEDS-1) - j;
+      } else {
+        pixelnumber = j;
+      }
+      leds[pixelnumber] = color;
+    }
+}
+
+void Black()
+{
+  CRGB black = CRGB::Black;
+
+  currentPalette = CRGBPalette16(
+    black, black, black, black,
+    black, black, black, black,
+    black, black, black, black,
+    black, black, black, black);
+}
+
+void SetupPurpleAndGreenPalette()
+{
+  CRGB purple = CHSV( HUE_PURPLE, 255, 255);
+  CRGB green  = CHSV( HUE_GREEN, 255, 255);
+  CRGB black  = CRGB::Black;
+ 
+  currentPalette = CRGBPalette16(
+    green,  green,  black,  black,
+    purple, purple, black,  black,
+    green,  green,  black,  black,
+    purple, purple, black,  black );
+}
+
+
---
+++ gui.py
@@ -0,0 +1,174 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import serial
+import time
+import os
+import sys
+import struct
+
+from tkinter import Tk, Text, W, E, N, S, LEFT, RIGHT, BOTH, HORIZONTAL, Scale
+from tkinter.ttk import Frame, Button, Style
+from tkinter.colorchooser import *
+
+clear = lambda: os.system('cls')
+arduino = serial.Serial('COM3', 9600)
+
+pal = "N/A"
+bright = 0
+speed = 20
+time.sleep(2)
+i = True
+clear()
+
+class LEDMeister(Frame):
+	def __init__(self):
+		super().__init__()
+
+		self.initUI()
+
+	def initUI(self):
+		def rainbow():
+			arduino.write(struct.pack('>BBBB', 2,1,0,0))
+			time.sleep(.1)
+			arduino.flush()
+		def baw():
+			arduino.write(struct.pack('>BBBB', 2,2,0,0))
+			time.sleep(.1)
+			arduino.flush()
+		def fire():
+			arduino.write(struct.pack('>BBBB', 2,3,0,0))
+			time.sleep(.1)
+			arduino.flush()
+		def lava():
+			arduino.write(struct.pack('>BBBB', 2,5,0,0))
+			time.sleep(.1)
+			arduino.flush()
+		def forest():
+			arduino.write(struct.pack('>BBBB', 2,6,0,0))
+			time.sleep(.1)
+			arduino.flush()
+		def ocean():
+			arduino.write(struct.pack('>BBBB', 2,4,0,0))
+			time.sleep(.1)
+			arduino.flush()
+		def police():
+			arduino.write(struct.pack('>BBBB', 2,8,0,0))
+			time.sleep(.1)
+			arduino.flush()
+		def party():
+			arduino.write(struct.pack('>BBBB', 2,9,0,0))
+			time.sleep(.1)
+			arduino.flush()
+		def customColor():
+			color = askcolor()
+			if color:
+				red = int(color[0][0])
+				green = int(color[0][1])
+				blue = int(color[0][2])
+				colors = (4, red, green, blue)
+				print(colors)
+				arduino.write(struct.pack('>BBBB', 4,red,green,blue))
+				time.sleep(.1)
+				arduino.flush()
+		def setbright():
+			arduino.write(struct.pack('>BBBB', 1,2,brightslider.get(),0))
+			time.sleep(.1)
+			arduino.flush()
+		def setspeed():
+			arduino.write(struct.pack('>BBBB', 1,4,speedslider.get(),0))
+			time.sleep(.1)
+			arduino.flush()
+		def savesettings():
+			arduino.write(struct.pack('>BBBB', 3,0,0,0))
+			time.sleep(.1)
+			arduino.flush()
+		def invertrotation():
+			arduino.write(struct.pack('>BBBB', 1,3,0,0))
+			time.sleep(.1)
+			arduino.flush()
+
+		arduino.write(struct.pack('>BBBB', 9,1,0,0))
+		got = arduino.readline()[:-2].decode('utf-8').split()
+		pal = got[0]
+		bright = got[1]
+		speed = got[2]
+		direction = got[3]
+		print(pal)
+		print(bright)
+		print(speed)
+		print(direction)
+		time.sleep(.1)
+		arduino.flush()
+
+		self.master.title("LED Meister")
+
+		self.columnconfigure(0, pad=3)
+		self.columnconfigure(1, pad=3)
+		self.columnconfigure(2, pad=3)
+		self.columnconfigure(3, pad=3)
+
+		self.rowconfigure(0, pad=3)
+		self.rowconfigure(1, pad=3)
+		self.rowconfigure(2, pad=3)
+		self.rowconfigure(3, pad=3)
+
+		rbbutton = Button(self, text="Rainbow", command=rainbow)
+		rbbutton.grid(row=1, column=0)
+
+		bwbutton = Button(self, text="B&W", command=baw)
+		bwbutton.grid(row=1, column=1)
+
+		firebutton = Button(self, text="Fire", command=fire)
+		firebutton.grid(row=1, column=2)
+
+		lavabutton = Button(self, text="Lava", command=lava)
+		lavabutton.grid(row=1, column=3)
+
+		forbutton = Button(self, text="Forest", command=forest)
+		forbutton.grid(row=2, column=0)
+
+		oceanbutton = Button(self, text="Ocean", command=ocean)
+		oceanbutton.grid(row=2, column=1)
+
+		polbutton = Button(self,text="Police", command=police)
+		polbutton.grid(row=2, column=2)
+
+		parbutton = Button(self,text="Party", command=party)
+		parbutton.grid(row=2, column=3)
+
+		colbutton = Button(self, text="Solid color", command=customColor)
+		colbutton.grid(row=3, column=0)
+
+		brightslider = Scale(self, from_=0, to=127, orient=HORIZONTAL, label="Brightness", showvalue=0, sliderlength=20, length=230, width=10)
+		brightslider.set(bright)
+		brightslider.grid(row=4, column=0, columnspan=3)
+
+		setbrightness = Button(self,text="Set", command=setbright)
+		setbrightness.grid(row=4, column=3)
+
+		speedslider = Scale(self, from_=25, to=255, orient=HORIZONTAL, label="Speed", showvalue=0, sliderlength=20, length=230, width=10)
+		speedslider.set(speed)
+		speedslider.grid(row=5, column=0, columnspan=3)
+
+		setspeedbut = Button(self,text="Set", command=setspeed)
+		setspeedbut.grid(row=5, column=3)
+
+		invertbutton = Button(self, text="Invert rot.", command=invertrotation)
+		invertbutton.grid(row=6, column=0)
+
+		savebutton = Button(self, text="Save settings", command=savesettings)
+		savebutton.grid(row=7, column=2)
+
+		quitbutton = Button(self, text="Quit", command=self.quit)
+		quitbutton.grid(row=7, column=3)
+
+		self.pack()
+
+def main():
+	root = Tk()
+	app = LEDMeister()
+	root.mainloop()
+
+if __name__ == '__main__':
+	main()
